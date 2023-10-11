ddb1a705a95769df98d0fda2a2ff54cc35709579
Louis Stafford <louiststafford@gmail.com>
2023-10-11 17:00:27 +0000

    Add assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual(70, 70.0);
+assertEqual(1, "1");
