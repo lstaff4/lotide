b44b27054a8f4b1437f1cabf354a4bba552f4161
Louis Stafford <louiststafford@gmail.com>
2023-10-14 00:22:28 +0000

    Add eqArray

---
+++ eqArrays.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  let eqArrResult = true;
+  let arrsLength = 0;
+  if (arr1.length > arr2.length) {
+    arrsLength = arr1.length;
+  } else {
+    arrsLength = arr2.length;
+  };
+  for (let i = 0; i < arrsLength; i++) {
+    if (arr1[i] !== arr2[i]) {
+      eqArrResult = false;
+    }
+  };
+  return eqArrResult
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
