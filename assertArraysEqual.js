d8c8c1ab9271f1be2bf8c24cb802db2c1a8a5e73
Louis Stafford <louiststafford@gmail.com>
2023-11-07 14:51:56 +0000

    Reformat assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function(array1, array2) {
-  assertEqual(eqArrays(array1, array2), true);
+const assertArraysEqual = function(array1, array2, boolean) {
+  assertEqual(eqArrays(array1, array2), boolean);
 };
 
 module.exports = assertArraysEqual;
