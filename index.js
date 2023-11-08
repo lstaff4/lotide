1f3c962fced151bb76bfd970c56698a3f6a2c80c
Louis Stafford <louiststafford@gmail.com>
2023-11-08 14:53:05 +0000

    Shorten index.js

--- index.js
+++ index.js
@@ -2,37 +2,37 @@
 const head = require('./head');
 const tail = require('./tail');
 const middle = require('./middle');
-const assertEqual = require('./assertEqual')
-const eqArrays = require('./eqArrays')
-const assertArraysEqual = require('./assertArraysEqual')
-const assertObjectsEqual = require('./assertObjectsEqual')
-const countLetters = require('./countLetters')
-const countOnly = require('./countOnly')
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
 const eqObjects = require('./eqObjects');
-const letterPositions = require('./letterPositions')
+const letterPositions = require('./letterPositions');
 const map = require('./map');
-const findKey = require('./findKey')
-const findKeyByValue = require('./findKeyByValue')
-const flatten = require('./flatten')
-const takeUntil = require('./takeUntil')
-const without = require('./without')
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertEqual: assertEqual,
-  eqArrays: eqArrays,
-  assertArraysEqual: assertArraysEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqObjects: eqObjects,
-  letterPositions: letterPositions,
-  map: map,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  flatten: flatten,
-  takeUntil: takeUntil,
-  without: without
+  head,
+  tail,
+  middle,
+  assertEqual,
+  eqArrays,
+  assertArraysEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqObjects,
+  letterPositions,
+  map,
+  findKey,
+  findKeyByValue,
+  flatten,
+  takeUntil,
+  without
 };
